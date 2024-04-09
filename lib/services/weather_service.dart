fc637fb6d2b8b932ba419b6e624f45c1dcb20941
Hasan Taha Künkül <tahaknkl2323@gmail.com>
2024-04-10 00:15:11 +0300

    service.dart commit

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:daily_weather/models/weather_model.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
@@ -20,4 +22,23 @@ class WeatherService {
       throw Exception('Failed to load weather data');
     }
   }
+
+Future<String> getCurrentCity() async {
+  LocationPermission permission = await Geolocator.requestPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+  }
+
+  Position position = await Geolocator.getCurrentPosition(
+    desiredAccuracy: LocationAccuracy.high,
+  );
+
+  List<Placemark> placemarks = await placemarkFromCoordinates(
+    position.latitude,
+    position.longitude,
+  );
+
+  String? city = placemarks.isNotEmpty ? placemarks[0].locality : null;
+  return city ?? 'Unknown City';
+}
 }
