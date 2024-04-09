5ed5a6139be37725aaec6ac817b2c8975474fbb7
Hasan Taha Künkül <tahaknkl2323@gmail.com>
2024-04-09 18:49:43 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:daily_weather/pages/weather_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+     
+      home: WeatherPage(),
+    );
+  }
+}
---
+++ lib/services/weather_service.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+
+import 'package:daily_weather/models/weather_model.dart';
+import 'package:http/http.dart' as http;
+
+class WeatherService {
+  static const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather';
+
+  final String apiKey;
+  WeatherService({
+    required this.apiKey,
+  });
+  Future<Weather> getWeather(String cityName) async {
+    final url = '$BASE_URL?q=$cityName&appid=$apiKey&units=metric';
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body);
+      return Weather.fromJson(json);
+    } else {
+      throw Exception('Failed to load weather data');
+    }
+  }
+}
